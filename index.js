18d554c8789f235f250a21c38722d0e25b3a3bc5
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-01-22 14:19:00 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// prompt user to list yogurt flavors
+// take user input and create array (split function)
+// observe how much of each flavor they have ordered
+// create loop 
+// create for in loop
+// use if statemnt to count each occurance of flavor
+
+
+  
+  const userOrder = prompt (`please enter your order separated by commas`);
+  const userOrderString = userOrder.split(`,`);
+  // console.log(userOrderString);
+  
+  //objects
+  const iceCream = {
+    name: ""
+  };
+  iceCream.name = userOrderString;
+let ArrayFlavorIceCream = [];
+//let emptyArray = [];
+
+//checks for a value like vanilla chocolate etc and only adds it once []
+//this adds it manually, other options are splice split
+
+ArrayFlavorIceCream = userOrder ();
+
+ // for (let i = 0; i <ArrayFlavorIceCream.length ; i++)
+  //  if (ArrayFlavorIceCream[0] === emptyArray[0]){
+  //    emptyArray = ArrayFlavorIceCream[0]
+      
+//ask in the empty array is any vanilla
+//if not then add vanilla
+//if yes skip hile stop
+
+//how to ask code to check each value 
+
+// split or splice array 
+//find finds it in an array
+
+      if ()
+  
+    }
+    //this only takes each flavor one time andskips the other
+
+  // console.log(iceCream.name);
+  
+
+
+  //for (let i = 0; i < userOrderString.length; i++) {}
+
+
+
+
+  //let userValue =  []; userValue = prompt;
+
+  //console.log(userValue);
